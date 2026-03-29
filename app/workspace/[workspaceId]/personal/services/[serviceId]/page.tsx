67994c21c6296baeff6b0b3a63cb645dc33ac87f
munashesydney <sydneymach1@gmail.com>
2026-03-29 12:38:50 -0700

    another minor fix for secrets page

--- app/workspace/[workspaceId]/personal/services/[serviceId]/page.tsx
+++ app/workspace/[workspaceId]/personal/services/[serviceId]/page.tsx
@@ -30,7 +30,7 @@ export default async function ServiceIdPage({ params }: PageProps) {
 
   const session = await auth();
   const userId = session?.user?.id;
-  let workspaceSecrets: { key: string; id: string; value?: string }[] = [];
+  let workspaceSecrets: { key: string; id: string; value: string }[] = [];
   if (userId) {
     const { data } = await getSecrets(workspaceId, userId, 1, 100);
     workspaceSecrets = data.map(s => ({ key: s.key, id: s.id, value: s.value }));
