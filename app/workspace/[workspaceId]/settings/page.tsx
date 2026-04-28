930222486b337a0de6526cbd6b98d98355e5a2d7
munashesydney <sydneymach1@gmail.com>
2026-04-27 21:00:33 -0700

    fixed build

--- app/workspace/[workspaceId]/settings/page.tsx
+++ app/workspace/[workspaceId]/settings/page.tsx
@@ -1,4 +1,5 @@
 import { requireWorkspaceOwner } from "@/lib/services/auth.service";
+import { getWorkspaceSettings } from "@/lib/services/workspace-settings.service";
 import { GeneralSettings } from "../../../../components/settings/general-settings";
 
 export default async function SettingsPage(props: {
@@ -6,12 +7,14 @@ export default async function SettingsPage(props: {
 }) {
   const { workspaceId } = await props.params;
   const { workspace } = await requireWorkspaceOwner(workspaceId);
+  const settings = await getWorkspaceSettings(workspaceId);
 
   return (
     <GeneralSettings
       workspaceId={workspaceId}
       initialName={workspace.name}
       initialDescription={workspace.description ?? ""}
+      initialTimezone={settings?.timezone ?? "UTC"}
     />
   );
 }
